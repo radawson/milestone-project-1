09be17ce9eb285aaee8fa6f98bf8a3da664ae430
Tara Maddox <TMaddox85@gmail.com>
2023-01-16 16:20:48 -0800

    ball moves left/right goes outside block

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
+let ball = document.querySelector("#ball");
+let moveBy = 10;
 
+//function moveLeft(){}
+//function moveRight(){}
+//listens for left and right key clicks
+window.addEventListener('load',() =>{
+    ball.style.position = 'relative';
+    ball.style.left = 0;
+    ball.style.top = 0;
+    });
+document.addEventListener('keyup',(e) =>{
+    switch(e.key){
+        case 'ArrowLeft':
+            ball.style.left = parseInt(ball.style.left )- moveBy + 'px';
+            break;
+        case 'ArrowRight':
+            ball.style.left = parseInt(ball.style.left) + moveBy + 'px';
+            break;
+    }
+    });
+//listens for unclicked to stop moving
+//document.addEventListener()
