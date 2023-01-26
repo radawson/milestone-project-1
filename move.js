716882356ce2b95dbcb6d0052293e607ed8b92b2
Rick Dawson <dawsora@gmail.com>
2023-01-25 20:57:59 -0500

    added falling ball mechanic and start button

--- move.js
+++ move.js
@@ -1,59 +1,62 @@
 function move(element) {
     element.style.position = 'absolute'
+    // fallFactor is the speed at which the ball falls
+    var fallFactor = 1;
 
     function moveToCoordinates(left, bottom) {
         element.style.left = left + 'px'
         element.style.bottom = bottom + 'px'
     }
 
-    function moveWithArrowKeys(left, bottom, callback){
+    function moveWithArrowKeys(left, bottom, callback) {
         let direction = null;
         let x = left;
         let y = bottom;
 
         element.style.left = x + 'px'
         element.style.bottom = y + 'px'
-        
-        function moveCharacter(){ 
-            if(direction === 'west'){
-                x-=1
+
+        function moveCharacter() {
+            if (direction === 'west') {
+                x -= 1
             }
-            if(direction === 'north'){
-                y+=1
+            if (direction === 'north') {
+                y += 1
             }
-            if(direction === 'east'){
-                x+=1
+            if (direction === 'east') {
+                x += 1
             }
-            if(direction === 'south'){
-                y-=1
+            if (direction === 'south') {
+                y -= 1
             }
+            y -= fallFactor;
             element.style.left = x + 'px'
             element.style.bottom = y + 'px'
         }
-        
+
         setInterval(moveCharacter, 1)
-        
-        document.addEventListener('keydown', function(e){
-            if(e.repeat) return;
-        
-            if(e.key === 'ArrowLeft'){
+
+        document.addEventListener('keydown', function (e) {
+            if (e.repeat) return;
+
+            if (e.key === 'ArrowLeft') {
                 direction = 'west'
             }
-            if(e.key === 'ArrowUp'){
+            /*if(e.key === 'ArrowUp'){
                 direction = 'north'
-            }
-            if(e.key === 'ArrowRight'){
+            }*/
+            if (e.key === 'ArrowRight') {
                 direction = 'east'
             }
-            if(e.key === 'ArrowDown'){
+            if (e.key === 'ArrowDown') {
                 direction = 'south'
             }
-            callback(direction)
+            //callback(element);
         })
-        
-        document.addEventListener('keyup', function(e){
-            direction = null
-            callback(direction)
+
+        document.addEventListener('keyup', function (e) {
+            direction = null;
+            //callback(element);
         })
     }
 
--- script.js
+++ script.js
@@ -6,18 +6,50 @@ class coordinate {
     }
 }
 
+// get message element for notifications
+const message = document.querySelector('#message');
+
+// range for walls to maintain reasonable space
+// enough for 4 walls
+let wallRange = [
+    40,240,
+    280, 520,
+    560, 800,
+    800, 960
+]
+
 // start with three walls
+// This could also be an array of arrays
 let wall1 = [];
 let wall2 = [];
 let wall3 = [];
+let wall4 = [];
+
+let ball = new coordinate(between(40, 460), 920);
 
 // build wall function
-function buildWall(wall) {
-    let y = between(40, 960);
+function buildWall(wall, index) {
+    let y = between(wallRange[index], wallRange[index +1]);
     for (let x = 0; x < 500; x += 20) {
         wall.push(new coordinate(x, y))
     }
 }
+
+// draw ball function
+function renderBall() {
+    move(newImage('assets/ball.png')).withArrowKeys(ball.x, ball.y, collision);
+}
+
+// collision function - could all be in the callback
+function collision(element){
+// first, if the ball gets to the bottom
+if (element.style.bottom == 0 ){
+
+}
+
+}
+
+
 
 // draw wall function
 function drawWall(wall) {
@@ -26,38 +58,43 @@ function drawWall(wall) {
     }
 }
 
-// initialize walls
-function initialize() {
-    buildWall(wall1);
-    buildWall(wall2);
-    buildWall(wall3);
-    // add random holes
-    // we know our walls are 25 blocks long
-    wall1.splice(between(0,25), 2);
-    wall2.splice(between(0,25), 2);
-    wall3.splice(between(0,25), 2);
-}
-
-function render() {
+// this is a separate function in case we need to redraw the walls
+function renderWalls() {
     drawWall(wall1);
     drawWall(wall2);
     drawWall(wall3);
+    drawWall(wall4);
 }
 
+// initialize walls
+function initialize() {
+    // format (wall, range index)
+    // range index numbers determine area wall will be displayed
+    buildWall(wall1, 0);
+    buildWall(wall2, 2);
+    buildWall(wall3, 4);
+    buildWall(wall4, 6);
+    // add random holes
+    // we know our walls are 25 blocks long
+    wall1.splice(between(0, 25), 2);
+    wall2.splice(between(0, 25), 2);
+    wall3.splice(between(0, 25), 2);
+    wall4.splice(between(0, 25), 2);
+    renderWalls(); // walls would not need to be rendered more than once at this point
+}
 
-
-// run game
-initialize();
 
 //game loop
 var count = 0;
-setInterval(mainLoop, 1000);
+//setInterval(mainLoop, 800); // if we need a timed game loop for gravity
 
 function mainLoop() {
-    render();
+    renderBall(); 
 }
 
-
+// run game
+initialize();
+//mainLoop();
 
 
 
